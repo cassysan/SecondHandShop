30c0187e1c91df96526078017998d592d19a051a
Cassy Myers <cassymyers@gmail.com>
2020-06-13 14:06:48 -0700

    adding in image gallery permission statements

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 import {
   StyleSheet,
@@ -10,9 +11,20 @@ import {
   ImageBackgroundComponent,
 } from "react-native";
 
+import Screen from "./app/components/Screen";
+
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ListingsEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <ListingsEditScreen />;
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestCameraRollPermissionsAsync();
+    if (!result.granted) {
+      alert("You need to enable Image Library Permissions");
+    }
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
